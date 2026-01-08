fe6fe6a1843230b9dc25da8eae4ebcd7e55c1103
rahat9016 <minhajurrohoman9016@gmail.com>
2026-01-08 10:50:24 +0600

    Add: DashboardHeader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "../components/ui/button";
 export default function Home() {
   return (
     <div className="container">
+      <DashboardHeader/>
       <Button>Click here</Button>
     </div>
   );
