668f74ba2088316e73580c037ebb61af56e9d3db
Minhazur Rahman <75428759+rahat9016@users.noreply.github.com>
2026-01-11 17:32:35 +0600

    Dev/minhaz (#9)
    * Add: Header & Footer
    
    * Add: Content add in the about page
    
    * Add: Responsive header
    
    * Add: Header & Footer
    
    * Fix: Build error

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import InternetStatus from "@/src/components/InternetStatus";
 import Footer from "@/src/components/layout/rootLayout/Footer";
 import Header from "@/src/components/layout/rootLayout/Header/Header";
+
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
